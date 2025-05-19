5cace159b6689434295004f80db3e99e80910f28
Arthur Albarelli <arthuralbarelli@gmail.com>
2025-05-18 21:41:30 -0300

    Fix Python identation

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,7 @@
 vim.opt.number = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.g.python_indent = {open_paren = 'shiftwidth()', closed_paren_align_last_line = false}
